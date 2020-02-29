39d6cff464df6f2870bb45e8070af86f15a48a6f
42yeah <potion@live.cn>
2020-02-29 11:23:27 +0800

    Initial commit

---
+++ desktop/src/org/fesilu/hinchin/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.fesilu.hinchin.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.fesilu.hinchin.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
