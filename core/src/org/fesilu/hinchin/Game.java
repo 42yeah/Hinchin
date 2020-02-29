77f0c1b4f94a45fb187fffe3b88bd4d9d8856ab8
42yeah <potion@live.cn>
2020-02-29 11:44:55 +0800

    All sorts of Chinese comments

--- core/src/org/fesilu/hinchin/Game.java
+++ core/src/org/fesilu/hinchin/Game.java
@@ -6,28 +6,71 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
+/**
+ * Game 是核心游戏类。游戏的渲染，创建，和扔掉都会在这里进行。
+ * Game 应该调用各种已经包装好的函数 / 类。其他写好的玩意儿应该无缝的衔接到 Game 中，
+ * 假如其他写好的玩意儿要通信，他们应该要比 Game 低一个等级。
+ */
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+	/**
+	 * create() 是在游戏创建的时候调用的第一个函数。
+	 * 其中应该包括材质加载，地图生成，还有各种值初始化啥的。
+	 */
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+
+		// 材质加载
 		img = new Texture("badlogic.jpg");
+
+		// 时间
+		lastInstant = System.currentTimeMillis();
 	}
 
+	/**
+	 * update() 是 Game 更新方法。每一帧都在 render() 最开始调用，
+	 * 更新的东西包括人物走动，饥饿减少之类。
+	 */
+	public void update() {
+		// 计算时差
+		long thisInstant = System.currentTimeMillis();
+		deltaTime = (float) (thisInstant - lastInstant) / 1000.0f;
+		lastInstant = thisInstant;
+	}
+
+	/**
+	 * render() 是 Game 的渲染方法。这是往屏幕上画东西的地方。
+	 * 一开始会调用 OpenGL 的清屏，之后会把各种材质渲染上去。
+	 */
 	@Override
 	public void render () {
+		// 更新游戏
+		update();
+		// 清除屏幕
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		// 开始绘画材质
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
+	/**
+	 * dispose() 是 Game 的退出方法。
+	 * 包括把材质扔掉，模型扔掉啥的。
+	 */
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
 	}
+
+	// deltaTime 是每一帧和上一帧的时差。这样可以保证不稳定的 FPS 下游戏一样正常运行。单位是秒。
+	private float deltaTime;
+	// lastInstant 是上一个瞬间的 System.currentTimeMillis. 用于计算毫秒级的 deltaTime。
+	private long lastInstant;
 }
--- desktop/src/org/fesilu/hinchin/desktop/DesktopLauncher.java
+++ desktop/src/org/fesilu/hinchin/desktop/DesktopLauncher.java
@@ -4,7 +4,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import org.fesilu.hinchin.Game;
 
+
 public class DesktopLauncher {
+	/**
+	 * main 是桌面构建的游戏主入口。从这里会初始化 Game, 之后就可以参考 core 中 Game 类的 create() 了。
+	 * @param arg 调用参数，咱可以不管
+	 */
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Game(), config);
